901aead6cbad60e666816d8da867725290d39c27
AlberteMB <albertebardanca@hotmail.com>
2025-05-03 18:39:26 +0200

    Config not completed in application.properties. Creating class for config.

---
+++ src/main/java/amb/config/DynamoDBConfig.java
@@ -0,0 +1,11 @@
+package amb.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDBConfig {
+
+}
+
