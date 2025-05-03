e64920f85af93a90ff4d5084907c82bad53c4684
AlberteMB <albertebardanca@hotmail.com>
2025-05-03 18:57:20 +0200

    Movie endpoint basic structure.

--- src/main/java/amb/movie/MovieEndpoint.java
+++ src/main/java/amb/movie/MovieEndpoint.java
@@ -1,4 +1,25 @@
 package amb.movie;
 
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.hilla.Endpoint;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Endpoint
+@AnonymousAllowed
 public class MovieEndpoint {
+
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieEndpoint(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    // Save movie
+    public void save(Movie movie) {
+        System.out.println("Saving movie: " + movie);
+        movieRepository.save(movie);
+    }
+
+
 }
