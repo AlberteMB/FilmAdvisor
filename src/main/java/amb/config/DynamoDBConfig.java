83e53621cf91f14f771f022e8b04056600576b60
AlberteMB <albertebardanca@hotmail.com>
2025-05-05 19:19:55 +0200

    Adding endpoint in DynamoDBConfig.

--- src/main/java/amb/config/DynamoDBConfig.java
+++ src/main/java/amb/config/DynamoDBConfig.java
@@ -1,31 +1,41 @@
 package amb.config;
 
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
 
 @Configuration
 public class DynamoDBConfig {
 
-//    @Value("${aws.accessKey}")
-//    private String accessKey;
-//
-//    @Value("${aws.secretKey}")
-//    private String secretKey;
+    @Value("${aws.dynamodb.endpoint}")
+    private String dynamodbEndpoint;
 
-    //@Value("${aws.region}")
-    @Value("${aws.region:eu-central-1}")
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.dynamodb.region}")
     private String region;
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+
         return DynamoDbClient.builder()
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+                .endpointOverride(URI.create(dynamodbEndpoint))
                 .region(Region.of(region))
                 .build();
     }
@@ -37,4 +47,3 @@ public class DynamoDBConfig {
                 .build();
     }
 }
-
