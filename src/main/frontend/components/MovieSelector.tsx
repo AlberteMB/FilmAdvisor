6fdda8b310946740578d97223a7a31d89baceada
AlberteMB <albertebardanca@hotmail.com>
2025-05-10 11:41:11 +0200

    useEffect with fetchMovies.

--- src/main/frontend/components/MovieSelector.tsx
+++ src/main/frontend/components/MovieSelector.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Grid, Container } from "@mui/material";
 import moviesData from "../data/movies.json";
 import MovieCard from "../components/MovieCard";
@@ -13,17 +13,31 @@ const MoviesSelector = () => {
     const [selectedMovies, setSelectedMovies] = useState<Movie[]>([]);
     const [numMovies, setNumMovies] = useState<number>(3);
 
+    useEffect(() => {
+        if (!triggerFetch) return;
+
+        const fetchMovies = async () => {
+          try {
+            const genre = selectedGenres.length > 0 ? selectedGenres[0].toUpperCase() : "ANY";
+            const movies = await MovieEndpoint.getFilteredRandomMovies(genre, selectedPlatforms, numMovies);
+            setSelectedMovies(movies);
+          } catch (error) {
+            console.error("Error fetching movies:", error);
+          } finally {
+            setTriggerFetch(false); // Reset para evitar llamadas repetidas
+          }
+        };
+
+        fetchMovies();
+      }, [triggerFetch, selectedGenres, selectedPlatforms, numMovies]);
+
     const handleSubmit = async () => {
         if (selectedPlatforms.length === 0) {
             alert("Selecciona al menos  una plataforma.");
             return;
           }
 
-        const movies = await MovieEndpoint.getFilteredRandomMovies(
-            selectedGenre.toUpperCase(),
-            selectedPlatforms
-        );
-        setSelectedMovies(movies);
+        setTriggerFetch(true);
     );
 
   return (
@@ -43,9 +57,9 @@ const MoviesSelector = () => {
       </label>
 
       {/* Button to get random movies */}
-      <<button onClick={handleSubmit} style={{ padding: "10px" }}>
-               Obtener películas
-             </button>
+        <<button onClick={handleSubmit} style={{ padding: "10px" }}>
+            Obtener películas
+        </button>
 
       {/* Container with movies */}
       <Container maxWidth="md" style={{ marginTop: "20px" }}>
