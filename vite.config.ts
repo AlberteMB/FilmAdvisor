7cbca0b68aabffe02c617e2a0d143af87e0ff1e4
AlberteMB <albertebardanca@hotmail.com>
2025-06-09 19:19:01 +0200

    Config dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { overrideVaadinConfig } from './vite.generated';
 const customConfig: UserConfigFn = (env) => ({
   // Here you can add custom Vite parameters
   // https://vitejs.dev/config/
+   root: './frontend',
 });
 
 export default overrideVaadinConfig(customConfig);
